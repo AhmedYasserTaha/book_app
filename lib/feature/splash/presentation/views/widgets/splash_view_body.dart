a6f5a9a3005472367dab2cc61609312701f2bbcf
AhmedYasserTaha <ayt614614@gmail.com>
2025-03-26 00:54:39 +0200

    finash splash screen      done

--- lib/feature/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/feature/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,26 @@
 import 'package:bookly_app/core/utils/assets_deta.dart';
+import 'package:bookly_app/feature/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:gap/gap.dart';
+import 'package:get/get.dart';
+import 'package:get/utils.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 3), () {
+      return Get.to(const HomeView(), transition: Transition.fade);
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +40,7 @@ class SplashViewBody extends StatelessWidget {
                     fontSize: 25,
                     fontWeight: FontWeight.bold,
                   ),
-                  speed: const Duration(milliseconds: 150),
+                  speed: const Duration(milliseconds: 100),
                 ),
               ],
               totalRepeatCount: 1,
