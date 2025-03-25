cd1e1d9d526fdfe3d0ace4956cd9ae8234c79e97
AhmedYasserTaha <ayt614614@gmail.com>
2025-03-26 00:46:13 +0200

    add text to splash view

--- lib/feature/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/feature/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:bookly_app/core/utils/assets_deta.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:gap/gap.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
@@ -10,7 +12,25 @@ class SplashViewBody extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [Image.asset(AssetsDeta.logo)],
+        children: [
+          Image.asset(AssetsDeta.logo),
+          const Gap(25),
+          Center(
+            child: AnimatedTextKit(
+              animatedTexts: [
+                TyperAnimatedText(
+                  'Read Free Books',
+                  textStyle: const TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  speed: const Duration(milliseconds: 150),
+                ),
+              ],
+              totalRepeatCount: 1,
+            ),
+          ),
+        ],
       ),
     );
   }
