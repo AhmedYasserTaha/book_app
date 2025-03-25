c058c9fcc005067b2f6a1ee0720ccb617ff27241
AhmedYasserTaha <ayt614614@gmail.com>
2025-03-26 00:29:01 +0200

    add assets

--- lib/feature/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/feature/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets_deta.dart';
 import 'package:flutter/material.dart';
 
 class SplashViewBody extends StatelessWidget {
@@ -5,6 +6,6 @@ class SplashViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(body: Column(children: [Image.asset(AssetsDeta.logo)]));
   }
 }
