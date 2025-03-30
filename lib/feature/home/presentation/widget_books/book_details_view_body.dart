ad291ac8652225143317f179e24b3ff8fe3348e2
AhmedYasserTaha <ayt614614@gmail.com>
2025-03-30 13:33:13 +0200

    create custom book to book details

--- lib/feature/home/presentation/widget_books/book_details_view_body.dart
+++ lib/feature/home/presentation/widget_books/book_details_view_body.dart
@@ -1,16 +1,28 @@
+import 'package:bookly_app/feature/home/presentation/views/widget/custom_list_view_item.dart';
 import 'package:bookly_app/feature/home/presentation/widget_books/custom_book_details_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
         body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 30),
-          child: Column(children: [CustomBookDetailsAppBar()]),
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            children: [
+              const CustomBookDetailsAppBar(),
+              const Gap(50),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.35,
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: const CustomListViewItem(),
+              ),
+            ],
+          ),
         ),
       ),
     );
