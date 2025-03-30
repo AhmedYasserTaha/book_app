356af3a241f166b329c558e1cccd862ef76905b9
AhmedYasserTaha <ayt614614@gmail.com>
2025-03-30 13:52:42 +0200

    craete box action

--- lib/feature/home/presentation/widget_books/book_details_view_body.dart
+++ lib/feature/home/presentation/widget_books/book_details_view_body.dart
@@ -1,9 +1,11 @@
 import 'package:bookly_app/core/constant/style.dart';
+import 'package:bookly_app/core/widget/custom_button_widget.dart';
 import 'package:bookly_app/feature/home/presentation/views/widget/book_rating.dart';
 import 'package:bookly_app/feature/home/presentation/views/widget/custom_list_view_item.dart';
 import 'package:bookly_app/feature/home/presentation/widget_books/custom_book_details_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
@@ -30,6 +32,33 @@ class BookDetailsViewBody extends StatelessWidget {
               const Gap(10),
               // ignore: prefer_const_constructors
               BookRating(mainAxisAlignment: MainAxisAlignment.center),
+              const Gap(40),
+              const Row(
+                children: [
+                  Expanded(
+                    child: CustomButtonWidget(
+                      backgroundColor: Colors.white,
+                      textColor: Colors.black,
+                      textTilte: "19.99\$",
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(12),
+                        bottomLeft: Radius.circular(12),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: CustomButtonWidget(
+                      backgroundColor: Color(0xffEF8262),
+                      textColor: Colors.white,
+                      textTilte: "Free Preview",
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(12),
+                        bottomRight: Radius.circular(12),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
