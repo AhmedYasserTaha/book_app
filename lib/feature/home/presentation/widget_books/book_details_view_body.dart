b344c8044cee7d610b8975c513ab31b806cea281
AhmedYasserTaha <ayt614614@gmail.com>
2025-04-01 23:25:23 +0200

    cmmit

--- lib/feature/home/presentation/widget_books/book_details_view_body.dart
+++ lib/feature/home/presentation/widget_books/book_details_view_body.dart
@@ -5,7 +5,6 @@ import 'package:bookly_app/feature/home/presentation/views/widget/custom_list_vi
 import 'package:bookly_app/feature/home/presentation/widget_books/custom_book_details_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:go_router/go_router.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
