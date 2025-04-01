1cbe06a3c01ae04c5f1c6310f0eaf9910443abf0
AhmedYasserTaha <ayt614614@gmail.com>
2025-04-02 01:07:50 +0200

    create bookEntity

--- lib/feature/home/domain/entities/books_entitie.dart
+++ lib/feature/home/domain/entities/books_entitie.dart
@@ -1,1 +1,13 @@
-class BooksEntitie {}
+class BooksEntitie {
+  final String name;
+  final String title;
+  final num price;
+  final num rating;
+
+  BooksEntitie({
+    required this.name,
+    required this.title,
+    required this.price,
+    required this.rating,
+  });
+}
