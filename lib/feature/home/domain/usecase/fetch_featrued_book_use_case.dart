4bf9602a5b5d99943911ec7576bb5a38fd7ddd7e
AhmedYasserTaha <ayt614614@gmail.com>
2025-04-03 14:21:50 +0200

    usecase

--- lib/feature/home/domain/usecase/fetch_featrued_book_use_case.dart
+++ lib/feature/home/domain/usecase/fetch_featrued_book_use_case.dart
@@ -1,14 +1,15 @@
-import 'package:bookly_app/core/errors/failure.dart';
-import 'package:bookly_app/feature/home/domain/entities/books_entitie.dart';
 import 'package:bookly_app/feature/home/domain/repo/home_repo.dart';
-import 'package:dartz/dartz.dart';
 
-class FetchFeatruedBookUseCase {
+class FetchFeatruedBookUseCase extends UseCase {
   final HomeRepo homeRepo;
 
-  FetchFeatruedBookUseCase({required this.homeRepo});
-
-  Future<Either<Failure, List<BooksEntity>>> fetchFteaturedBooks() {
-    return homeRepo.fetchFteaturedBooks();
+  @override
+  Future call() {
+    // TODO: implement call
+    throw UnimplementedError();
   }
+}
+
+abstract class UseCase {
+  Future call();
 }
