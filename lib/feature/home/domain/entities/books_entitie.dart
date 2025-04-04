4e55b35ab2b26da0a0f1da1dbb5e277c14c0cf3a
AhmedYasserTaha <ayt614614@gmail.com>
2025-04-04 23:56:45 +0200

    genrate book entity adabter

--- lib/feature/home/domain/entities/books_entitie.dart
+++ lib/feature/home/domain/entities/books_entitie.dart
@@ -1,17 +1,33 @@
-class BooksEntity {
+import 'package:hive/hive.dart'; // استخدم hive بدل hive_flutter هنا
+part 'books_entitie.g.dart';
+
+@HiveType(typeId: 0)
+class BooksEntity extends HiveObject {
+  // أضفت HiveObject للوراثة
+  @HiveField(0)
   final String bookId;
+
+  @HiveField(1)
   final String? image;
+
+  @HiveField(2)
   final String title;
+
+  @HiveField(3)
   final String? authorName;
+
+  @HiveField(4)
   final num? price;
+
+  @HiveField(5)
   final num? rating;
 
   BooksEntity({
     required this.bookId,
-    required this.image,
+    this.image, // غيرت required لأنه nullable
     required this.title,
-    required this.price,
-    required this.rating,
-    required this.authorName,
+    this.authorName, // غيرت required لأنه nullable
+    this.price, // غيرت required لأنه nullable
+    this.rating, // غيرت required لأنه nullable
   });
 }
