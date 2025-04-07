64b6ff6480ea71ba655bb0b30a49179b0071df3c
AhmedYasserTaha <ayt614614@gmail.com>
2025-04-08 00:11:38 +0200

    create news book cubit and dont state && cubit

---
+++ lib/feature/home/presentation/manegr/news_books_cubit/news_books_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/feature/home/domain/entities/books_entitie.dart';
+import 'package:bookly_app/feature/home/domain/usecase/fetch_nwes_books_use_case.dart';
+import 'package:flutter/widgets.dart';
+
+part 'news_books_state.dart';
+
+class NewsBooksCubit extends Cubit<NewsBooksState> {
+  NewsBooksCubit(this.fetchFNewsBooksUseCase) : super(NewsBooksInitial());
+  final FetchFNewsBooksUseCase fetchFNewsBooksUseCase;
+
+  Future<void> fetchNewsBooks() async {
+    emit(NewsBooksLoading());
+    var result = await fetchFNewsBooksUseCase.call();
+
+    result.fold(
+      (failure) {
+        emit(NewsBooksFailure(failure.errMessage));
+      },
+      (books) {
+        emit(NewsBooksSuccess(books));
+      },
+    );
+  }
+}
---
+++ lib/feature/home/presentation/manegr/news_books_cubit/news_books_state.dart
@@ -0,0 +1,20 @@
+part of 'news_books_cubit.dart';
+
+@immutable
+sealed class NewsBooksState {}
+
+final class NewsBooksInitial extends NewsBooksState {}
+
+final class NewsBooksLoading extends NewsBooksState {}
+
+final class NewsBooksSuccess extends NewsBooksState {
+  final List<BooksEntity> books;
+
+  NewsBooksSuccess(this.books);
+}
+
+final class NewsBooksFailure extends NewsBooksState {
+  final String errMessage;
+
+  NewsBooksFailure(this.errMessage);
+}
