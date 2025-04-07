a61d1ca0a49d4adb4518e93ab1ad0624c08cae24
AhmedYasserTaha <ayt614614@gmail.com>
2025-04-07 23:57:23 +0200

    FeatureBooksCubit  done

--- lib/feature/home/presentation/manegr/featured_books_cubit/feature_books_cubit.dart
+++ lib/feature/home/presentation/manegr/featured_books_cubit/feature_books_cubit.dart
@@ -1,9 +1,27 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/feature/home/domain/entities/books_entitie.dart';
-import 'package:meta/meta.dart';
+import 'package:bookly_app/feature/home/domain/usecase/fetch_featrued_book_use_case.dart';
+import 'package:flutter/widgets.dart';
 
 part 'feature_books_state.dart';
 
 class FeatureBooksCubit extends Cubit<FeatureBooksState> {
-  FeatureBooksCubit() : super(FeatureBooksInitial());
+  FeatureBooksCubit(this.featruedBookUseCase) : super(FeatureBooksInitial());
+
+  final FetchFeatruedBookUseCase featruedBookUseCase;
+
+  Future<void> fetchFteaturedBooks() async {
+    emit(FeatureBooksLoading());
+
+    var result = await featruedBookUseCase.call();
+
+    result.fold(
+      (failure) {
+        emit(FeatureBooksFailure(failure.errMessage));
+      },
+      (books) {
+        FeatureBooksSuccess(books);
+      },
+    );
+  }
 }
